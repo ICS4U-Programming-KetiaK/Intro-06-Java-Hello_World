3f8d1a8c4b5bc51ccfd21ac95d88fceeff162a3d
Ubuntu <ubuntu@ip-172-31-49-187.ec2.internal>
2022-02-17 18:31:37 +0000

    created a hello world program.

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+* Created a Hello World program that displays
+* "Hello, World!" as the output to the terminal.
+*
+* @author Ketia Gaelle Kaze
+* @version 1.0
+* @since 2022-02-17
+*/
+class HelloWorld {
+    /**
+     * Displays text to terminal.
+     *
+     * @param args nothing passed in
+     */
+    private static void main(String[]args) {
+        System.out.println("Hello, World!");
+    }
+}
+
